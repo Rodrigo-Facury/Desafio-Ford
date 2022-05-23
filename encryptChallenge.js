cdc866590ef217df604df07940416226aa2baba4
Rodrigo <rodrigo.facury14@gmail.com>
2022-05-23 11:28:56 -0300

    Desafio resolvido!

---
+++ encryptChallenge.js
@@ -0,0 +1,48 @@
+const VIN_EXAMPLE = '4Y1SL65848Z411439';
+
+const SECRET_KEY = 'Ford';
+
+class Cryptography {
+  encrypt(vin, key) {
+    let encrypted = '';
+
+    const vinCharacters = vin.split('');
+    const keyCharacters = key.split('');
+
+    vinCharacters.forEach((char, index) => {
+      const vinCharCode = char.charCodeAt();
+
+      const analogueChar = keyCharacters[index % keyCharacters.length];
+
+      const result = vinCharCode + analogueChar.charCodeAt();
+
+      encrypted += String.fromCharCode(result);
+    });
+
+    return encrypted;
+  }
+
+  decrypt(cypher, key) {
+    let decrypted = '';
+
+    const vinCharacters = cypher.split('');
+    const keyCharacters = key.split('');
+
+    vinCharacters.forEach((char, index) => {
+      const vinCharCode = char.charCodeAt();
+
+      const analogueChar = keyCharacters[index % keyCharacters.length];
+
+      const result = vinCharCode - analogueChar.charCodeAt();
+
+      decrypted += String.fromCharCode(result);
+    });
+
+    return decrypted;
+  }
+}
+
+
+const cypher = new Cryptography().encrypt(VIN_EXAMPLE, SECRET_KEY);
+
+console.log(new Cryptography().decrypt(cypher, SECRET_KEY));
